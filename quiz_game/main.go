0df4be5c0f72254897a9ab51c003e88780943d35
Thomas <thomasgran@thomass-macbook-pro.home>
2020-01-04 22:44:50 +0100

    added new project

--- quiz_game/main.go
+++ quiz_game/main.go
@@ -6,11 +6,14 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
 	csvFilename := flag.String("csv", "problems.csv", "csv file in the format of 'question, answer'")
+	timeLimit := flag.Int("limit", 30, "Time limit in seconds for the quiz")
 	flag.Parse()
+
 	file, err := os.Open(*csvFilename)
 	if err != nil {
 		exit(fmt.Sprintf("Not able to open csv file %s \n", *csvFilename))
@@ -21,18 +24,30 @@ func main() {
 		exit(fmt.Sprintf("Failed to parse the provided csv file"))
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	//<-timer.C
 
 	correct := 0
 
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == p.a {
-			fmt.Println("Correct")
-			correct++
-		} else {
-			fmt.Println("Not correct!")
+		fmt.Printf("Problem #%d: %s = ", i+1, p.q)
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correct, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.a {
+				fmt.Println("Correct")
+				correct++
+			} else {
+				fmt.Println("Not correct!")
+			}
 		}
 	}
 	fmt.Printf("You scored %d out of %d.\n", correct, len(problems))
