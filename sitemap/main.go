df02f50b8067ab6b4cea3dfb0b4c1a82f0e35ef6
Implocell <thomaskgran@gmail.com>
2020-01-13 21:36:26 +0100

    added todos

--- sitemap/main.go
+++ sitemap/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Print("HELLO WORLD")
+	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to build a sitemap for")
+	flag.Parse()
+
+	fmt.Print(urlFlag)
 }
+
+/*
+	1. GET the webpage
+	2. parse all the links on the webpage
+	3. build proper urls with our links
+	4. filter out any links that have a diff domain
+	5. find all the pages (BFS)
+	6. print out XML
+
+*/
