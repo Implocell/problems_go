de7b35574194b563934fc3319a1530e5d93a327c
Thomas <thomasgran@thomass-macbook-pro.home>
2020-01-21 22:07:57 +0100

    almost done with sitemap

--- sitemap/main.go
+++ sitemap/main.go
@@ -3,13 +3,72 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"problems_go/link"
+	"strings"
 )
 
 func main() {
 	urlFlag := flag.String("url", "https://gophercises.com", "The url that you want to build a sitemap for")
 	flag.Parse()
 
-	fmt.Print(urlFlag)
+	fmt.Print(*urlFlag)
+
+	pages := get(*urlFlag)
+	for _, page := range pages {
+		fmt.Println(page)
+	}
+}
+
+func hrefs(r io.Reader, base string) []string {
+	links, _ := link.Parse(r)
+	var ret []string
+	for _, l := range links {
+		switch {
+		case strings.HasPrefix(l.Href, "/"):
+			ret = append(ret, base+l.Href)
+		case strings.HasPrefix(l.Href, "http"):
+			ret = append(ret, l.Href)
+		}
+	}
+	return ret
+}
+
+func get(urlStr string) []string {
+	resp, err := http.Get(urlStr)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	reqUrl := resp.Request.URL
+
+	baseUrl := &url.URL{
+		Scheme: reqUrl.Scheme,
+		Host:   reqUrl.Host,
+	}
+	base := baseUrl.String()
+
+	return filter(hrefs(resp.Body, base), withPrefix(base))
+
+}
+
+func filter(links []string, keepFn func(string) bool) []string {
+	var ret []string
+	for _, link := range links {
+		if keepFn(link) {
+			ret = append(ret, link)
+		}
+	}
+	return ret
+}
+
+func withPrefix(pfx string) func(string) bool {
+	return func(link string) bool {
+		return strings.HasPrefix(link, pfx)
+	}
 }
 
 /*
